5029a5806e16d9009e032450440d0248777f6191
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-10-29 21:45:05 -0300

    V0

--- client_rpc.py
+++ client_rpc.py
@@ -1,9 +1,5 @@
 import xmlrpc.client
 
-s = xmlrpc.client.ServerProxy('localhost:8001')
-print(s.pow(2,3))  # Returns 2**3 = 8
-# print(s.add(2,3))  # Returns 5
-# print(s.print("Olá, 10 anos!"))  # Returns 5*2 = 10
-
-# Print list of available methods
-# print(s.system.listMethods())
+with xmlrpc.client.ServerProxy("http://localhost:8001/") as proxy:
+    print(proxy.autenticarUsuario(122))
+    print(proxy.autenticarUsuario(130))
--- server_rpc.py
+++ server_rpc.py
@@ -1,32 +1,14 @@
 from xmlrpc.server import SimpleXMLRPCServer
-from xmlrpc.server import SimpleXMLRPCRequestHandler
 
-# Restrict to a particular path.
-class RequestHandler(SimpleXMLRPCRequestHandler):
-    rpc_paths = ('/RPC2',)
+port = 8001
 
-# Create server
-with SimpleXMLRPCServer(('localhost', 8000),
-                        requestHandler=RequestHandler) as server:
-    server.register_introspection_functions()
-    print("Listening on port 8000...")
+def autenticarUsuario(userID):
+    if(userID == 122 or userID == 123):
+        return "Usuário autenticado!"
+    else:
+        return "Usuário não encontrado :("
 
-    # Register pow() function; this will use the value of
-    # pow.__name__ as the name, which is just 'pow'.
-    server.register_function(pow)
-
-    # Register a function under a different name
-    def adder_function(x, y):
-        return x + y
-    server.register_function(adder_function, 'add')
-
-    # Register an instance; all the methods of the instance are
-    # published as XML-RPC methods (in this case, just 'mul').
-    class MyFuncs:
-        def mul(self, x, y):
-            return x * y
-
-    server.register_instance(MyFuncs())
-
-    # Run the server's main loop
-    server.serve_forever()
+server = SimpleXMLRPCServer(("localhost", port))
+print("Servidor do keeper rodando na porta %i..." %(port))
+server.register_function(autenticarUsuario, "autenticarUsuario")
+server.serve_forever()
